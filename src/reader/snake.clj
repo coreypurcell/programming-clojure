1fbf2cc364d3d4f2b7ae96c9bc5bac43b6fb004f
Corey Purcell <corey.purcell@livingsocial.com>
2015-02-07 11:50:41 -0500

    move added

--- src/reader/snake.clj
+++ src/reader/snake.clj
@@ -8,3 +8,35 @@
 ; END: namespace
 
 ; TODO: implement the Snake!
+(def width 75)
+(def height 50)
+(def point-size 10)
+(def turn-millis 75)
+(def win-length 5)
+(def dirs { VK_LEFT [-1 0]
+           VK_RIGHT [1 0]
+           VK_UP [0 -1]
+           VK_DOWN [0 1]})
+
+(defn add-points [& pts]
+  (vec (apply map + pts)))
+
+(defn point-to-screen-rect [pt]
+  (map #(* point-size %)
+       [(pt 0) (pt 1) 1 1]))
+
+(defn create-apple []
+  {:location [(rand-int width) (rand-int height)]
+   :color (Color. 210 50 90)
+   :type :apple})
+
+(defn create-snake []
+  {:body (list [1 1])
+   :dir [1 0]
+   :type :snake
+   :color (Color. 15 160 70)})
+
+(defn move [{:keys [body dir] :as snake} & grow]
+  (assoc snake :body (cons (add-points (first body) dir)
+                           (if grow body (butlast body)))))
+
